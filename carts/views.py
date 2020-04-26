d38560880396930908e50720a10ead44b304d330
konstantongl <konstantingl156@gmail.com>
2020-04-26 15:44:06 +0300

    Personal cart for every user + total cart count

--- carts/views.py
+++ carts/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, redirect
 from menu.models import product, topping, subs_extra
 from carts.models import cart, cart_item
+from account.models import Profile
 from django.http import HttpResponse
 # Create your views here.
 
 def cart_view(request):
-    the_cart = cart.objects.all()[0]
+    cart_owner = Profile.objects.get(user=request.user)
+    the_cart, created = cart.objects.get_or_create(owner=cart_owner)
+
+    # Cart total count
+    total = 0
+    for i in the_cart.products.all():
+        print (i.price)
+        total += i.price
+        print(total)
+    the_cart.total = total
+
     context = {'cart': the_cart}
     return render(request, 'carts/cart.html', context)
 
 def update_cart(request, id):
-    select = request.GET.get('size_select')
-    the_cart = cart.objects.all()[0]
+    cart_owner = Profile.objects.get(user=request.user)
+    the_cart, created = cart.objects.get_or_create(owner=cart_owner)
     try:
         item = product.objects.get(id=id)
     except DoesNotExist:
         pass
     cart_obj, created = cart_item.objects.get_or_create(product=item, cart_one=the_cart)
 
+    select = request.GET.get('size_select')
     if select == 'small':
         cart_obj.price = item.small_price
         cart_obj.save()
--- menu/views.py
+++ menu/views.py
@@ -1,17 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from menu.models import product, topping, subs_extra
 
 # Create your views here.
 def index(request):
-    context = {
-    "regular_pizza": product.objects.filter(category='Regular Pizza'),
-    "sicilian_pizza": product.objects.filter(category='Sicilian Pizza'),
-    "subs": product.objects.filter(category='Sub'),
-    "pastas": product.objects.filter(category='Pasta'),
-    "salads": product.objects.filter(category='Salad'),
-    "dinners": product.objects.filter(category='Dinner'),
-    "toppings": topping.objects.all(),
-    "subs_extras": subs_extra.objects.all(),
-    }
-    return render(request, 'orders/index.html', context)
+    if request.user.is_authenticated:
+        context = {
+        "regular_pizza": product.objects.filter(category='Regular Pizza'),
+        "sicilian_pizza": product.objects.filter(category='Sicilian Pizza'),
+        "subs": product.objects.filter(category='Sub'),
+        "pastas": product.objects.filter(category='Pasta'),
+        "salads": product.objects.filter(category='Salad'),
+        "dinners": product.objects.filter(category='Dinner'),
+        "toppings": topping.objects.all(),
+        "subs_extras": subs_extra.objects.all(),
+        }
+        return render(request, 'orders/index.html', context)
+
+    else:
+        return redirect('login')
